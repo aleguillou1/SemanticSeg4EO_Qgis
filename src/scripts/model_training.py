2ee0c6383893610c20f4886ad571186d0e78f07b
Adrien <76942394+aleguillou1@users.noreply.github.com>
2026-05-08 12:18:09 +0200

    Add copyright and license comments
    Added copyright and licensing information to model_training.py

--- src/scripts/model_training.py
+++ src/scripts/model_training.py
@@ -22,6 +22,17 @@ Features:
 
 Usage:
     python model_training.py params.json progress.json
+
+    Copyright (c) 2026 Le Guillou Adrien
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
 """
 
 import os
